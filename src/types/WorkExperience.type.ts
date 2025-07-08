e0881ed18840deeaa1fb4857480d1d61964aa7c9
Jose Calancha <daksonvibes38@gmail.com>
2025-07-08 16:38:48 -0400

    initial commit

---
+++ src/types/WorkExperience.type.ts
@@ -0,0 +1,9 @@
+import type { AstroComponentFactory } from "astro/runtime/server/index.js";
+
+export interface WorkExperienceType {
+  place: string;
+  acronym: string;
+  description: string;
+  duration: string;
+  logo: AstroComponentFactory;
+}

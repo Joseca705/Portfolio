afb81a6706b22a65f124492c3bf6bad8da60273f
Jose Calancha <daksonvibes38@gmail.com>
2025-07-11 16:26:16 -0400

    Update conf of astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Joseca705.github.io',
-  base: 'Portfolio',
+  site: 'https://joseca705.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
